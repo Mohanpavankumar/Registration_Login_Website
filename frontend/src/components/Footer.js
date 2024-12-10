e80adb56aeb36fbd385860a668fc53e973d7f056
V.Mohanapavankumar <mohanpavankumarvankayala@gmail.com>
2024-12-09 21:54:29 -0600

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,6 +5,7 @@ const Footer = () => {
     <footer className='bg-slate-200'>
       <div className='container mx-auto p-4'>
         <p className='text-center' title="Mohan's Website">Coding With Mohan❤️❤️❤️</p>
+        <p className='text-center'>Contact</p>
       </div>
     </footer>
   )
