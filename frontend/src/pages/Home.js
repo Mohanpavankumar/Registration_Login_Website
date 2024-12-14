2bb246e8e55f7db9cd0ac2416cb4291683edfa72
V.Mohanapavankumar <mohanpavankumarvankayala@gmail.com>
2024-12-14 11:38:52 -0600

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div classname = 'flex w-full max-w-xl mx-auto justify-center'>
+    <div classname = 'flex w-full max-w-xl mx-auto justify-center text-blue-600'>
       Welcome to Mern Authentication System
     </div>
   )
