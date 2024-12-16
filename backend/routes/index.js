e744097a6cfd01e60ba458a180ec43663682f0f8
V.Mohanapavankumar <mohanpavankumarvankayala@gmail.com>
2024-12-16 08:54:17 -0600

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
 const userSignUpController = require("../controller/userSignUp.js");
 const userSignInController = require('../controller/userSignIn.js');
 const userDetailsController = require('../controller/userDetails.js');
@@ -7,9 +7,9 @@ const authToken = require('../middleware/authToken.js');
 const userLogout = require('../controller/userLogout.js');
 
 
-router.post("/signup",userSignUpController)
-router.post("/signin",userSignInController)
-router.get("/user-details", authToken, userDetailsController)
-router.get("/userLogout",userLogout)
+router.post("/signup",userSignUpController);
+router.post("/signin",userSignInController);
+router.get("/user-details", authToken, userDetailsController);
+router.get("/userLogout",userLogout);
 
-module.exports = router
+module.exports = router;
