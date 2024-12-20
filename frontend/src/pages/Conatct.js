870ee942dcf248badd5fbedb4a9485ade91f2baf
V.Mohanapavnkumar <mohanpavankumarvankayala@gmail.com>
2024-12-20 10:25:20 -0600

    Changes in Contact Info

--- frontend/src/pages/Conatct.js
+++ frontend/src/pages/Conatct.js
@@ -4,38 +4,36 @@ const Conatct = () => {
   return (
     <div>
       <form className='p-5 flex flex-col gap-2' onSubmit={handleSubmit}>
-                <div className='grid'>
-                    <label>Email :</label>
-                    <div className='bg-slate-100 p-2'>
-                        <input type='email' 
-                        placeholder='enter email'
-                        name= 'email'
-                        value={data.email}
-                        onChange={handleOnChange}
-                        required 
-                        className='w-full h-full outline-none bg-transparent'/>
-                    </div>
-                </div>
-
-                <div>
-                    <label>Password :</label>
-                    <div className='bg-slate-100 p-2 flex'>
-                        <input 
-                        type={showPassword ? "password" : "text"}
-                        placeholder='enter password'
-                        name='password'
-                        value={data.password}
-                        onChange={handleOnChange}
-                        required 
-                        className='w-full h-full outline-none bg-transparent'/>
-                    </div>
-                    <Link to={'/forgot-Password'} className='block w-fit ml-auto hover:underline hover:text-blue-600 '>
-                        Submit
-                    </Link>
-                </div>
-
-                <button className='bg-blue-600 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 hover:bg-blue-800 transition-all mx-auto block mt-8'>Login</button>
-            </form>
+        <div>
+          <label>Name :</label>
+          <div className='bg-slate-100 p-2 flex'>
+              <input 
+              type='name'
+              placeholder='enter name'
+              name='name'
+              value={data.name}
+              onChange={handleOnChange}
+              required 
+              className='w-full h-full outline-none bg-transparent'/>
+          </div>
+          <Link to={'/forgot-Password'} className='block w-fit ml-auto hover:underline hover:text-blue-600 '>
+              Submit
+          </Link>
+        </div>
+        <div className='grid'>
+            <label>Email :</label>
+            <div className='bg-slate-100 p-2'>
+                <input type='email' 
+                placeholder='enter email'
+                name= 'email'
+                value={data.email}
+                onChange={handleOnChange}
+                required 
+                className='w-full h-full outline-none bg-transparent'/>
+            </div>
+        </div>
+        <button className='bg-blue-600 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 hover:bg-blue-800 transition-all mx-auto block mt-8'>Send</button>
+      </form>
     </div>
   )
 }
