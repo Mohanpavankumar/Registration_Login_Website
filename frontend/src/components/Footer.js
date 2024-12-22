b966b8bad9e0eaaba6f3e8133ded47442893bb9c
V.Mohanapavankumar <mohanpavankumarvankayala@gmail.com>
2024-12-22 13:53:52 -0600

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,10 +3,11 @@ import React, { Link } from 'react';
 
 const Footer = () => {
   return (
-    <footer className='bg-slate-200'>
+    <footer className='bg-slate-200 flex justify-center items-center'>
       <div className='container mx-auto p-4'>
         <p className='text-center' title="Mohan's Website">Coding With Mohan❤️❤️❤️</p>
         <Link to={'/contact'} className='text-center'>Contact</Link>
+        <p>*copy rights*</p>
       </div>
     </footer>
   )
