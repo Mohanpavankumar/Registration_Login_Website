01c90403cbc29d182587520db7fd64c03c5c46b7
V.Mohanapavnkumar <mohanpavankumarvankayala@gmail.com>
2025-01-02 12:32:35 -0600

    Changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI)
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000, // Increase timeout
+          })
     }catch(err){
         console.log(err)
     }
