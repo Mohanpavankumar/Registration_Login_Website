3d0ed6c0053d0c78f7f6d57bed47281bf5f29df0
V.Mohanapavnkumar <mohanpavankumarvankayala@gmail.com>
2025-01-02 22:37:59 -0600

    Changes

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <footer className='bg-slate-200 flex justify-between items-center'>
-      <div className='container mx-auto p-4'>
+      <div className='flex justify-between container mx-auto p-4'>
         <p className='text-center' title="Mohan's Website">Coding With Mohan❤️❤️❤️</p>
-        <Link to={'/contact'} className='text-center'>Contact</Link>
+        <p><Link to={'/contact'} className='text-center'>Contact</Link></p>
         <p className='text-center'>&copy; 2024 Mern Authentication System</p>
       </div>
     </footer>
