5483dec8c4d785e7f6967c05362c1a1fb075d10b
V.Mohanapavnkumar <mohanpavankumarvankayala@gmail.com>
2025-01-04 08:42:43 -0600

    Changes

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -20,14 +20,14 @@ const Contact = () => {
   const handleSubmit = async(e) =>{
     e.preventDefault();
     
-    const dataResponse = await fetch(SummaryApi.Contact.url,{
-        method : SummaryApi.Contact.method,
-        credentials : "include",
-        headers : {
-            "content-type" : "application/json"
-        },
-        body : JSON.stringify(formData)
-    })
+  const dataResponse = await fetch(SummaryApi.Contact.url,{
+      method : SummaryApi.Contact.method,
+      credentials : "include",
+      headers : {
+          "content-type" : "application/json"
+      },
+      body : JSON.stringify(formData)
+  })
 
   const dataApi = await dataResponse.json();
 
@@ -48,39 +48,39 @@ const Contact = () => {
             <span className='flex text-blue-600 font-bold justify-center'>Contact Form</span> 
             <form className='p-5 flex flex-col gap-2' onSubmit={handleSubmit}>
               <div className='grid'>
-                  <label>Name :</label>
-                  <div className='bg-slate-100 p-2'>
-                      <input type='text' 
-                      placeholder='enter name'
-                      name= 'name'
-                      value={formData.name}
-                      onChange={handleChange}
-                      required 
-                      className='w-full h-full outline-none bg-transparent'/>
-                  </div>
+                <label>Name :</label>
+                <div className='bg-slate-100 p-2'>
+                    <input type='text' 
+                    placeholder='enter name'
+                    name= 'name'
+                    value={formData.name}
+                    onChange={handleChange}
+                    required 
+                    className='w-full h-full outline-none bg-transparent'/>
+                </div>
               </div>
               <div className='grid'>
-                  <label>Email :</label>
-                  <div className='bg-slate-100 p-2'>
-                      <input type='email' 
-                      placeholder='enter email'
-                      name='email'
-                      value={formData.email}
-                      onChange={handleChange}
-                      required 
-                      className='w-full h-full outline-none bg-transparent'/>
-                  </div>
+                <label>Email :</label>
+                <div className='bg-slate-100 p-2'>
+                    <input type='email' 
+                    placeholder='enter email'
+                    name='email'
+                    value={formData.email}
+                    onChange={handleChange}
+                    required 
+                    className='w-full h-full outline-none bg-transparent'/>
+                </div>
               </div>
               <div>
-                  <label>Message :</label>
-                  <div className='bg-slate-100 p-2'>
-                      <input type='text' 
-                      placeholder='enter message'
-                      name= 'message'
-                      value={formData.message}
-                      onChange={handleChange}
-                      className='w-full h-full outline-none bg-transparent'/>
-                  </div>
+                <label>Message :</label>
+                <div className='bg-slate-100 p-2'>
+                    <input type='text' 
+                    placeholder='enter message'
+                    name= 'message'
+                    value={formData.message}
+                    onChange={handleChange}
+                    className='w-full h-full outline-none bg-transparent'/>
+                </div>
               </div>
               <button type='submit' className='bg-blue-600 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 hover:bg-blue-800 transition-all mx-auto block mt-8'>Send</button>
             </form>
