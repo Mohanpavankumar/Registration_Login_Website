fd6e02e73df8e2a0d13ee9222cd16c869c34f785
V.Mohanapavnkumar <mohanpavankumarvankayala@gmail.com>
2025-01-04 08:45:49 -0600

    Changes

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className='bg-slate-200 flex justify-between items-center'>
       <div className='flex justify-between container mx-auto p-4'>
-        <p className='text-center' title="Mohan's Website">Coding With Mohan❤️❤️❤️</p>
+        <p className='text-center' title="Mohan's Website">Coding With Mohan</p>
         <p><Link to={'/contact'} className='text-center'>Contact</Link></p>
         <p className='text-center'>&copy; 2024 Mern Authentication System</p>
       </div>
