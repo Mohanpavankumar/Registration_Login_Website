125b7c7d0610cd05e3d6bfc87882c1f4c05ca9b0
V.Mohanapavnkumar <mohanpavankumarvankayala@gmail.com>
2025-01-05 13:15:48 -0600

    Changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI)
-    }catch(err){
-        console.log(err)
+        await mongoose.connect(process.env.MONGODB_URI);
+    }catch(error){
+        console.log(error);
     }
 }
 
-module.exports = connectDB
+module.exports = connectDB;
