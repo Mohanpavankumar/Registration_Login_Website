47d019510e9ec4d51cc960bd7b5c08bad06ce9a5
V.Mohanapavnkumar <mohanpavankumarvankayala@gmail.com>
2025-01-07 20:59:22 -0600

    Changes

--- frontend/src/EndPoints/index.js
+++ frontend/src/EndPoints/index.js
@@ -1,26 +1,26 @@
-const backendDomain = "http://localhost:8000";
+const backendDomain = process.env.REACT_APP_BACKEND_URL || "http://localhost:8000";
 
 const SummaryApi = {
     signUp: {
-        url : `${backendDomain}/api/signup`,
-        method : "post"
+        url: `${backendDomain}/api/signup`,
+        method: "post"
     },
     signIn: {
-        url : `${backendDomain}/api/signin`,
-        method : "post"
+        url: `${backendDomain}/api/signin`,
+        method: "post"
     },
     current_user: {
-        url : `${backendDomain}/api/user-details`,
-        method : "get"
+        url: `${backendDomain}/api/user-details`,
+        method: "get"
     },
     logout_user: {
-        url : `${backendDomain}/api/userLogout`,
-        method : "get"
+        url: `${backendDomain}/api/userLogout`,
+        method: "get"
     },
     Contact: {
-        url : `${backendDomain}/api/contact`,
-        method : "post"
+        url: `${backendDomain}/api/contact`,
+        method: "post"
     }
-}
+};
 
 export default SummaryApi;
